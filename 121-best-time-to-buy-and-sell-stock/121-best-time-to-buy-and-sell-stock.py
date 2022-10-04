40b58bc1c379802eca1b36564a3d4042e62edd74
Shrey Agrawal <69215650+shrey8761@users.noreply.github.com>
2022-10-05 00:54:33 +0530

    Updated  5/9/2022 at 0:54, Easy, Day 3

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left=0
+        right=1
+        max_profit=0
+
+        while right<len(prices):
+            Corenet=prices[right]-prices[left]
+
+            if prices[left]<prices[right]:
+                max_profit=max(Corenet,max_profit)
+
+            else:
+                left=right
+            right+=1
+        return max_profit
