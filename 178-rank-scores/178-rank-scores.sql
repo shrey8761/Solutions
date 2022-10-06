54e8cf97c15501169bb12645cf1dcf8b96afd052
Shrey Agrawal <69215650+shrey8761@users.noreply.github.com>
2022-10-06 21:53:34 +0530

    Updated  6/9/2022 at 21:53, Medium, Day 4

---
+++ 178-rank-scores/178-rank-scores.sql
@@ -0,0 +1,14 @@
+SELECT
+  S1.score,
+  (
+    SELECT
+      COUNT(DISTINCT S2.score)
+    FROM
+      Scores S2
+    WHERE
+      S2.score >= S1.score
+  ) AS 'rank'
+FROM
+  Scores S1
+ORDER BY
+  S1.score DESC;
