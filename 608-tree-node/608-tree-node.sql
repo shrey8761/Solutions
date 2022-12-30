7d3f4c844c4f2171ee6fe587acb2a19ffe954480
Shrey Agrawal <69215650+shrey8761@users.noreply.github.com>
2022-12-30 19:20:44 +0530

    Updated  30/11/2022 at 19:20, Medium, Day 5

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -1,9 +1,10 @@
-SELECT DISTINCT t1.id,
-    CASE 
-        WHEN t1.p_id IS NULL THEN 'Root' ELSE
-        CASE
-            WHEN t1.id = t2.p_id THEN 'Inner' ELSE 'Leaf'
-        END
-    END AS 'Type'
-    FROM Tree t1 LEFT JOIN Tree t2 ON t1.id = t2.p_id
-    ORDER BY id
+select 
+    id as "id",
+    case
+    when t.p_id is null then "Root"
+    when t.id in(select p_id from Tree)then "Inner"
+    else "Leaf" 
+    end as "type"
+    
+from   Tree t
+    
