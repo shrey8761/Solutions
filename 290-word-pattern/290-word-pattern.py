01b6d81465a61c9ccd4a0cf46c60c43a4f857fb2
Shrey Agrawal <69215650+shrey8761@users.noreply.github.com>
2023-01-04 16:48:15 +0530

    Updated  4/0/2023 at 16:48, Easy, Day 3

--- 290-word-pattern/290-word-pattern.py
+++ 290-word-pattern/290-word-pattern.py
@@ -1,5 +1,17 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        wp = s.split(' ')
+        wp = s.split()
+        if len(pattern) != len(wp):
+            return False
 
-        return [*map(wp.index, wp)] == [*map(pattern.index, pattern)]
+        d = {}
+        for i in range(len(pattern)):
+            if pattern[i] not in d: #this means key not there.
+                if wp[i] in d.values(): #this means key is not , but value is there.
+                     return False
+                d[pattern[i]] = wp[i]
+            elif d[pattern[i]] != wp[i]:
+                return False
+        return True
+            
+        
