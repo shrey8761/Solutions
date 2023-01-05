5f042a0a3feafbd0be0a82382370378d793f4237
Shrey Agrawal <69215650+shrey8761@users.noreply.github.com>
2023-01-05 20:45:17 +0530

    Updated  5/0/2023 at 20:45, Medium, Day 4

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -1,35 +1,11 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        
-        n = len(matrix)
-        
-        dp= []
-        for i in range(0,n):
-            arr = []
-            for j in range(0,n):
-                arr.append(-1)
-            dp.append(arr)
-        
-        mini = float(inf)
-        for i in range(n):
-            mini = min (mini, findmin(0,i,matrix,dp))
-        return mini
-    
-def findmin(cr,cc, matrix ,dp):
-    n = len(matrix[0])
-    if cr>n-1 or cc > n-1 or cc < 0 or cr <0:
-        return float(inf)
-
-    if cr == n-1:
-        return matrix[cr][cc]
-
-    if dp[cr][cc] != -1:
-        return dp[cr][cc]
-
-    down = findmin(cr+1,cc,matrix,dp)
-    downleft = findmin(cr+1,cc-1,matrix,dp)
-    downright = findmin(cr+1,cc+1,matrix,dp)
-    dp[cr][cc] = matrix[cr][cc] + min(down , min(downleft,downright))
-    return dp[cr][cc]
-    
-    
+        for row in range(1,len(matrix)):
+            for col in range(len(matrix)):
+                temp = matrix[row-1][col]
+                if col-1 >= 0:
+                    temp = min(temp,matrix[row-1][col-1])
+                if col+1 < len(matrix):
+                    temp = min(temp,matrix[row-1][col+1])
+                matrix[row][col] += temp
+        return min(matrix[-1])
