446abc0c8e87db8d9423ce2977f57b4624940b03
Shrey Agrawal <69215650+shrey8761@users.noreply.github.com>
2023-01-29 22:52:03 +0530

    Updated  29/0/2023 at 22:51, Easy, Day 0

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -6,6 +6,26 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#     This is first way
+        # if root is None:
+        #     return 0
+        # return 1+(max(self.maxDepth(root.left),self.maxDepth(root.right)))
+        
+# This is seconf way
         if root is None:
             return 0
-        return 1+(max(self.maxDepth(root.left),self.maxDepth(root.right)))
+        
+        d = deque([root])
+        ans = 0
+        while d:
+            for i in range(len(d)):
+                node = d.pop()
+                if node.left:
+                    d.appendleft(node.left)
+                if node.right:
+                    d.appendleft(node.right)
+            ans += 1
+        return ans
+            
+            
+        
