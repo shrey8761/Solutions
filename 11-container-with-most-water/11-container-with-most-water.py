4ea9a0bb712a9fc82acc5eff633afdaaf874f540
Shrey Agrawal <69215650+shrey8761@users.noreply.github.com>
2023-02-09 23:55:37 +0530

    Updated  9/1/2023 at 23:55, Medium, Day 4

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,12 +1,22 @@
 class Solution:
-    def maxArea(self, H: List[int]) -> int:
-        ans, i, j = 0, 0, len(H)-1
-        while (i < j):
-            if H[i] <= H[j]:
-                res = H[i] * (j - i)
-                i += 1
+    def maxArea(self, h: List[int]) -> int:
+#         working brute force
+        # res = 0
+        # ans = 0 
+        # for i in range(len(height)):
+        #     for j in range(i,len(height)):
+        #         ans = (j-i)*(min(height[i],height[j]))
+        #         res = max(res,ans)
+        # return res
+        
+#         OPTIMIZED PICHINIg Window 
+        maxwater = 0 # Initialize maximum variable to 0
+        l, r = 0, len(h)-1 # Define initial window of maximum size
+        while l < r:
+            if h[l] <= h[r]:#Find smaller height and shift its pointer inwards
+                maxwater = max(maxwater, (r-l) * h[l])
+                l+=1
             else:
-                res = H[j] * (j - i)
-                j -= 1
-            if res > ans: ans = res
-        return ans
+                maxwater = max(maxwater, (r-l) * h[r])
+                r-=1
+        return maxwater
