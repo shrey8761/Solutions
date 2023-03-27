6a5f0eddd0b6c3097b298dd8d717e07e68f59c79
Shrey Agrawal <69215650+shrey8761@users.noreply.github.com>
2023-03-27 21:08:12 +0530

    Updated  27/2/2023 at 21:8, Medium, Day 1

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,22 +1,17 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        memo = [[-1 for i in range(len(grid[0]))] for i in range(len(grid))]
-
-        def find_sum(grid: List[List[int]], memo: List[List[int]], row: int, col: int) -> int:
-            row_max = len(grid)
-            col_max = len(grid[0])
-
-            if row >= row_max or col >= col_max:
-                return 0xFFFF
-
-            if row == row_max - 1 and col == col_max - 1:
-                return grid[row][col]
-
-            if memo[row][col] != -1:
-                return memo[row][col]
-
-            memo[row][col] = grid[row][col] + min(find_sum(grid, memo, row, col + 1), find_sum(grid, memo, row + 1, col))
-
-            return memo[row][col]
-
-        return find_sum(grid, memo, 0, 0)
+            
+        
+        m, n = len(grid), len(grid[0])
+        
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+        
+        for i in range(1, n):
+            grid[0][i] += grid[0][i-1]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        
+        return grid[-1][-1]
