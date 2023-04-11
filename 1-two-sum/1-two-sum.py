f5116ff91d53c4fbee560eeedca7b48a84793d28
Shrey Agrawal <69215650+shrey8761@users.noreply.github.com>
2023-04-11 17:29:57 +0530

    Updated  11/3/2023 at 17:29, Easy, Day 2

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        dict1 = {}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i,j]
-                
-                
+            comp = target - nums[i]
+            if comp in dict1:
+                return [dict1[comp],i]
+            dict1[nums[i]] = i
+        return []
