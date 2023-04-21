41a1667c7582a0da1daaa4a9ba702adb88644da1
Shrey Agrawal <69215650+shrey8761@users.noreply.github.com>
2023-04-22 01:40:14 +0530

    Updated  22/3/2023 at 1:40, Medium, Day 6

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,20 +1,23 @@
 class Solution:
     def numDecodings(self,s): 
-        if not s:
+# def num_ways_to_decode(s):
+    # Check if the input string is empty or starts with 0
+        if not s or s[0] == '0':
             return 0
 
-        dp = [0 for x in range(len(s) + 1)] 
+        n = len(s)
+        dp = [0] * (n+1)
+        dp[0] = 1 # There is only one way to decode an empty string
+        dp[1] = 1 # There is only one way to decode a single-digit string
 
-        # base case initialization
-        dp[0] = 1 
-        dp[1] = 0 if s[0] == "0" else 1   #(1)
+        for i in range(2, n+1):
+            # If the current digit can be decoded by itself, we can add the number of ways to decode the previous digit
+            if s[i-1] != '0':
+                dp[i] += dp[i-1]
 
-        for i in range(2, len(s) + 1): 
-            # One step jump
-            if 0 < int(s[i-1:i]) <= 9:    #(2)
-                dp[i] += dp[i - 1]
-            # Two step jump
-            if 10 <= int(s[i-2:i]) <= 26: #(3)
-                dp[i] += dp[i - 2]
-        return dp[len(s)]
-        
+            # If the current digit and the previous digit can be decoded as a single letter, we can add the number of ways to decode the two digits before the current digit
+            two_digits = int(s[i-2:i])
+            if 10 <= two_digits <= 26:
+                dp[i] += dp[i-2]
+
+        return dp[n] # Return the number of ways to decode the entire string
